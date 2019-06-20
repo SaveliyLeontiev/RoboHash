d81e4b756168f79c848a18048f36e37106914860
Saveliy Leontyev <saveliy.leontyev@noveogroup.com>
2019-06-20 18:33:00 +0700

    UI imaplementation

--- RoboHash/RoboImageModule/RoboImageViewController.swift
+++ RoboHash/RoboImageModule/RoboImageViewController.swift
@@ -8,12 +8,28 @@ import UIKit
 class RoboImageViewController: UIViewController {
     private let roboImagePresenter = RoboImagePresenter()
 
+    @IBOutlet private weak var nameField: UITextField!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var generateButton: UIButton!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+        configurePresenter()
     }
 }
 
 private extension RoboImageViewController {
+    func configureView() {
+        nameField.delegate = self
+        nameField.autocapitalizationType = .none
+        nameField.autocorrectionType = .no
+        generateButton.addTarget(self, action: #selector(generateButtonAction(_:)),
+                                 for: .touchUpInside)
+        setLoading(false)
+    }
+
     func configurePresenter() {
         roboImagePresenter.delegate = self
     }
@@ -21,4 +37,24 @@ private extension RoboImageViewController {
 
 // MARK: - RoboImagePresenterDelegate
 extension RoboImageViewController: RoboImagePresenterDelegate {
+}
+
+// MARK: - UITextFieldDelegate
+extension RoboImageViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+}
+
+// MARK: - Actions
+extension RoboImageViewController {
+    @objc
+    func generateButtonAction(_ sender: Any) {
+        print("TODO: Add generation action")
+    }
+
+    private func setLoading(_ isLoading: Bool) {
+        activityIndicator.isHidden = !isLoading
+        imageView.alpha = isLoading ? 0.2 : 1
+    }
 }
